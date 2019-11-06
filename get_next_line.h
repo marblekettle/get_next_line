d401884b2980a7f727bb0896684abb3451a9e476
Brendan sylvain robin Mans <bmans@f0r5s6.codam.nl>
2019-11-06 13:28:38 +0100

    Created functions and added subject

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bmans <marvin@codam.nl>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/06 13:11:18 by bmans         #+#    #+#                 */
+/*   Updated: 2019/11/06 13:15:30 by bmans         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 256
+# endif
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+
+#endif
