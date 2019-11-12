d1abecb46d016046df87ec9cf2fbc109551583d1
marblekettle <tencivanni@gmail.com>
2019-11-13 00:58:54 +0100

    Read functionality

---
+++ Makefile
@@ -0,0 +1,16 @@
+NAME = gnl.out
+SRCS = get_next_line.c get_next_line_utils.c
+INCLUDE = .
+
+all: $(NAME)
+
+$(NAME):
+	gcc get_next_line.c get_next_line_utils.c -o $(NAME) -D BUFFER_SIZE=10
+
+clean:
+
+fclean: clean
+	rm -rf $(NAME)
+
+re: fclean all
+
--- get_next_line.c
+++ get_next_line.c
@@ -11,8 +11,42 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
 
 int		get_next_line(int fd, char **line)
 {
+	unsigned int	i;
+	char			c;
+	char			buf[BUFFER_SIZE];
+
+	i = 0;
+	c = 1;
+	while (c != '\n' && c != '\0')
+	{
+		read(fd, &c, 1);
+		if (c == '\n' || c == '\0')
+			break ;
+		buf[i] = c;
+		i++;
+		if (i == BUFFER_SIZE)
+		{
+			allocat(line, buf);
+			i = 0;
+		}
+	}
+	buf[i] = '\0';
+	allocat(line, buf);
+	return (c ? 1 : 0);
+}
+
+int		main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("./test.txt", O_RDWR);
+	while(get_next_line(fd, &line)
+		printf("%s\n", line);
 	return (0);
 }
